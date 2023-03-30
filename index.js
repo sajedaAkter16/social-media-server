e72231736c4237582a0f9c6a4d0429b18d3be40c
Sajeda Akter <sajeda16akter@gmail.com>
2023-03-31 00:34:36 +0600

    'user-info-added'

--- index.js
+++ index.js
@@ -1,40 +1,41 @@
-const express = require('express')
-const app = express()
-const cors=require('cors')
-require('dotenv').config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 // middleware
-    app.use(cors());
-    app.use(express.json())
-    
-app.get('/', (req, res) => {
-  res.send('Hello  Social Media!')
-})
+app.use(cors());
+app.use(express.json());
 
-
+// mongodb connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nvo4nkj.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run(){
-    const postCollection=client.db('social-media').collection('posts')
+  try{
+    const userCollection=client.db('social_media').collection('users'); // wait ok
 
-    try{
+    // users info
+    app.post('/users', async(req,res)=>{
+      const user=req.body;
+    
+      const response = await userCollection.insertOne(user);
+      console.log(response);
+    })
+  }
+  finally{
 
-        app.post('/posts',async(req,res)=>{
-            const post=req.body;
-            const allPost=await postCollection.insertOne(post);
-            res.send(allPost)
-        })
-    }
-    finally{
-
-    }
-
+  }
 }
-run().catch(console.log)
+run().catch(err=>console.log(err))
+
+
+app.get("/", (req, res) => {
+  res.send("Hello  Social Media!");
+});
 
 app.listen(port, () => {
-  console.log(`social media app listening on port ${port}`)
-})
+  console.log(`social media app listening on port ${port}`);
+});
